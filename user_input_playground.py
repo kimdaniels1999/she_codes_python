d1ea51d2ab0652b144d1b69c79ea870b77565bd7
kimdaniels1999 <86605048+kimdaniels1999@users.noreply.github.com>
2021-07-10 09:18:51 +1000

    Update user_input_playground.py

--- user_input_playground.py
+++ user_input_playground.py
@@ -3,6 +3,26 @@
 # message = f"I'm {answer} as well, thanks for asking"
 # print(message)
 
-name = input("What is your name?")
-hobby = input("What is your hobby?")
-print(f"This is {name} and I like {hobby}")
+# name = input("What is your name?")
+# hobby = input("What is your hobby?")
+# print(f"This is {name} and I like {hobby}")
+# print(type(f"This is {name} and I like {hobby}"))
+
+# age = input (f"Hi {name}, how old are you?")
+# years = 100 - int(age)
+# print(type(years))
+# print(f"Wow {name}! You'll be 100 in {years} years!")
+
+# first_number = input("Which number comes after 2 =")
+# second_number = input("Name the number before 10 =")
+# sum = int(first_number) + int(second_number)
+# # print(sum)
+# print(f"Great, add these together and this makes {sum}")
+# negative = input("What is 2 - 5 =")
+# second_number2 = input("Name the number before 10 =")
+# sum = int(negative) + int(second_number2)
+# print(f"Great, add these together and this makes {sum}")
+choose = input("Choose the correct answer, what number goes between 2.0 and 4.0 =")
+second_number3 = input("what is -12 - 3 = ")
+
+
